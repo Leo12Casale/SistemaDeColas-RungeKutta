2c0cf04e46892465bf259ee8a7cf964ba71944f9
LeonelCasale <leonelcasale@gmail.com>
2022-06-28 12:04:53 -0300

    Tabla RK

--- TP Final/Modelo/RungeKutta.cs
+++ TP Final/Modelo/RungeKutta.cs
@@ -12,146 +12,99 @@ namespace Sistemas_de_Colas.Modelo
         private string tipo;
         private double h;
 
-        public const String proximoBloqueo = "proximo_bloqueo";
-        public const String finBloqueoLlegada = "bloqueo_llegada";
-        public const String finBloqueoServidor = "bloqueo_servidor";
+        public const String unTrabajo = "1 trabajo";
+        public const String dosTrabajos = "2 trabajos";
 
         public DataTable tabla;
 
         public RungeKutta(string tipo)
         {
-            this.tipo = tipo;
-            this.h = 0.1;
+            this.h = 0.01;
         }
 
-        public double integracionNumerica(double reloj, double rndB = 1, double tiempoA = 1) //rndB es opcional (solo se usa para instante_bloqueo)
+        public double integracionNumerica(double reloj, string tipo) //rndB es opcional (solo se usa para instante_bloqueo)
         {
             Fila fila = new Fila();
 
-            //Instante inicial t0
+            //Instante inicial M(0)=100
             fila.Tiempo = 0;
-            fila.VarDependiente = Math.Truncate(1000 * reloj) / 1000; //A, L o S dependiendo qué ecuación dif. se use
+            fila.IndiceSecado = 100;
 
             if (tabla == null)
                 tabla = crearTabla();
 
-            tabla.Rows.Add("Reloj:" + Math.Truncate(1000 * reloj) / 1000);
-            switch (this.tipo)
+            tabla.Rows.Add("Reloj:" + truncar(1000 * reloj) / 1000);
+            switch (tipo)
             {
-                case proximoBloqueo:
-                    fila.VarDependiente = Math.Truncate(1000 * tiempoA) / 1000;
+                case unTrabajo:
                     while (true)
                     {
-                        fila.K1 = Math.Truncate(1000 * ecuacionDiferencialInstanteBloqueo(fila.VarDependiente, rndB)) / 1000;
-                        fila.K2 = Math.Truncate(1000 * ecuacionDiferencialInstanteBloqueo(fila.VarDependiente + h / 2 * fila.K1, rndB)) / 1000;
-                        fila.K3 = Math.Truncate(1000 * ecuacionDiferencialInstanteBloqueo(fila.VarDependiente + h / 2 * fila.K2, rndB)) / 1000;
-                        fila.K4 = Math.Truncate(1000 * ecuacionDiferencialInstanteBloqueo(fila.VarDependiente + h * fila.K3, rndB)) / 1000;
-                        fila.TiempoSiguiente = Math.Truncate(1000 * (fila.Tiempo + h)) / 1000;
-                        fila.VarDependienteSiguiente = Math.Truncate(1000 * (fila.VarDependiente + (h / 6) * (fila.K1 + 2 * fila.K2 + 2 * fila.K3 + fila.K4))) / 1000;
+                        fila.K1 = truncar(1000 * ecuacionDiferencialUnTrabajo(fila.Tiempo, fila.IndiceSecado));
+                        fila.K2 = truncar(1000 * ecuacionDiferencialUnTrabajo(fila.Tiempo + h/2, fila.IndiceSecado + h/(2*fila.K1)));
+                        fila.K3 = truncar(1000 * ecuacionDiferencialUnTrabajo(fila.Tiempo + h/2, fila.IndiceSecado + h/(2*fila.K2)));
+                        fila.K4 = truncar(1000 * ecuacionDiferencialUnTrabajo(fila.Tiempo + h, fila.IndiceSecado + h*fila.K3));
+                        fila.TiempoSiguiente = truncar(1000 * (fila.Tiempo + h));
+                        fila.IndiceSecadoSiguiente = truncar(1000 * (fila.IndiceSecado + (h/6) * (fila.K1 + 2*fila.K2 + 2*fila.K3 + fila.K4)));
 
-                        agregarFilaTabla(fila, proximoBloqueo);
+                        agregarFilaTabla(fila);
 
-                        if (fila.VarDependiente >= 2 * reloj)
+                        if (fila.IndiceSecado <  1)
                         {
                             tabla.Rows.Add();
-                            return fila.Tiempo * 9;
+                            return fila.Tiempo;
                         }
 
                         //Para fila siguiente
                         fila.Tiempo = fila.TiempoSiguiente;
-                        fila.VarDependiente = fila.VarDependienteSiguiente;
+                        fila.IndiceSecado = fila.IndiceSecadoSiguiente;
                     }
 
-                case finBloqueoLlegada: 
-                    double varDependienteAnterior = 0;
+                case dosTrabajos: 
                     while (true)
                     {
-                        fila.K1 = Math.Truncate(1000 * ecuacionDiferencialBloqueoLlegada(fila.Tiempo, fila.VarDependiente)) / 1000;
-                        fila.K2 = Math.Truncate(1000 * ecuacionDiferencialBloqueoLlegada(fila.Tiempo + h / 2, fila.VarDependiente + h / 2 * fila.K1)) / 1000;
-                        fila.K3 = Math.Truncate(1000 * ecuacionDiferencialBloqueoLlegada(fila.Tiempo + h / 2, fila.VarDependiente + h / 2 * fila.K2)) / 1000;
-                        fila.K4 = Math.Truncate(1000 * ecuacionDiferencialBloqueoLlegada(fila.Tiempo + h, fila.VarDependiente + h * fila.K3)) / 1000;
-                        fila.TiempoSiguiente = Math.Truncate(1000 * (fila.Tiempo + h)) / 1000;
-                        fila.VarDependienteSiguiente = Math.Truncate(1000 * (fila.VarDependiente + (h / 6) * (fila.K1 + 2 * fila.K2 + 2 * fila.K3 + fila.K4))) / 1000;
+                        fila.K1 = truncar(ecuacionDiferencialDosTrabajos(fila.Tiempo, fila.IndiceSecado));
+                        fila.K2 = truncar(1000 * ecuacionDiferencialDosTrabajos(fila.Tiempo + h/2, fila.IndiceSecado + h/(2*fila.K1)));
+                        fila.K3 = truncar(1000 * ecuacionDiferencialDosTrabajos(fila.Tiempo + h/2, fila.IndiceSecado + h/(2*fila.K2)));
+                        fila.K4 = truncar(1000 * ecuacionDiferencialDosTrabajos(fila.Tiempo + h, fila.IndiceSecado + h*fila.K3));
+                        fila.TiempoSiguiente = truncar(1000 * (fila.Tiempo + h));
+                        fila.IndiceSecadoSiguiente = truncar(1000 * (fila.IndiceSecado + (h/6) * (fila.K1 + 2 * fila.K2 + 2 * fila.K3 + fila.K4)));
 
-                        agregarFilaTabla(fila, finBloqueoLlegada);
+                        agregarFilaTabla(fila);
 
-                        if (Math.Abs(varDependienteAnterior - fila.VarDependiente) < 1)
+                        if (fila.IndiceSecado < 1)
                         {
                             tabla.Rows.Add();
-                            return fila.Tiempo * 5;
-                        }
-
-                        //Para fila siguiente
-                        varDependienteAnterior = fila.VarDependiente;
-                        fila.Tiempo = fila.TiempoSiguiente;
-                        fila.VarDependiente = fila.VarDependienteSiguiente;
-                    }
-
-                case finBloqueoServidor: 
-                    while (true)
-                    {
-                        fila.K1 = Math.Truncate(1000 * ecuacionDiferencialBloqueoServidores(fila.Tiempo, fila.VarDependiente)) / 1000;
-                        fila.K2 = Math.Truncate(1000 * ecuacionDiferencialBloqueoServidores(fila.Tiempo + h / 2, fila.VarDependiente + h / 2 * fila.K1)) / 1000;
-                        fila.K3 = Math.Truncate(1000 * ecuacionDiferencialBloqueoServidores(fila.Tiempo + h / 2, fila.VarDependiente + h / 2 * fila.K2)) / 1000;
-                        fila.K4 = Math.Truncate(1000 * ecuacionDiferencialBloqueoServidores(fila.Tiempo + h, fila.VarDependiente + h * fila.K3)) / 1000;
-                        fila.TiempoSiguiente = Math.Truncate(1000 * (fila.Tiempo + h)) / 1000;
-                        fila.VarDependienteSiguiente = Math.Truncate(1000 * (fila.VarDependiente + (h / 6) * (fila.K1 + 2 * fila.K2 + 2 * fila.K3 + fila.K4))) / 1000;
-
-                        agregarFilaTabla(fila, finBloqueoServidor);
-
-                        if (fila.VarDependiente >= 1.35 * reloj)
-                        {
-                            tabla.Rows.Add();
-                            return fila.Tiempo * 2;
+                            return fila.Tiempo;
                         }
 
                         //Para fila siguiente
                         fila.Tiempo = fila.TiempoSiguiente;
-                        fila.VarDependiente = fila.VarDependienteSiguiente;
+                        fila.IndiceSecado = fila.IndiceSecadoSiguiente;
                     }
                 default:
                     return 0;
             }
         }
 
-        public double ecuacionDiferencialInstanteBloqueo(double varDependiente, double rndB)
+        public double ecuacionDiferencialUnTrabajo(double tiempo, double indiceSecado)
         {
-            return rndB * varDependiente;
+            return -0.05 * indiceSecado -0.0001 * tiempo;
         }
 
-        public double ecuacionDiferencialBloqueoLlegada(double tiempo, double varDependiente)
+        public double ecuacionDiferencialDosTrabajos(double tiempo, double indiceSecado)
         {
-            return -((varDependiente / 0.8) * Math.Pow(tiempo, 2)) - varDependiente;
+            return -0.05 * indiceSecado + 0.04 - 0.0001 * tiempo;
         }
 
-        public double ecuacionDiferencialBloqueoServidores(double tiempo, double varDependiente)
+        private double truncar(double numero)
         {
-            return (0.2 * varDependiente) + 3 - tiempo;
+            return Math.Truncate(1000 * numero) / 1000;
         }
 
         private DataTable crearTabla()
         {
             DataTable tabla = new DataTable();
-            string colVarDependiente = "";
-            string colVarDependienteSiguiente = "";
-            switch (this.tipo)
-            {
-                case proximoBloqueo:
-                    colVarDependiente = "A";
-                    colVarDependienteSiguiente = "A(t+i)";
-                    break;
-                case finBloqueoLlegada:
-                    colVarDependiente = "L";
-                    colVarDependienteSiguiente = "L(t+i)";
-                    break;
-                case finBloqueoServidor:
-                    colVarDependiente = "S";
-                    colVarDependienteSiguiente = "S(t+i)";
-                    break;
-                default:
-                    break;
-            }
-            string[] columnas = new string[] { "t", colVarDependiente, "K1", "K2", "K3", "K4", "t(i+1)", colVarDependienteSiguiente };
+            string[] columnas = new string[] { "t", "M", "K1", "K2", "K3", "K4", "t(i+1)", "M(t+i)"};
             for (int i = 0; i < columnas.Length; i++)
             {
                 tabla.Columns.Add(columnas[i]);
@@ -159,30 +112,30 @@ namespace Sistemas_de_Colas.Modelo
             return tabla;
         }
 
-        private void agregarFilaTabla(Fila fila, string tipo)
+        private void agregarFilaTabla(Fila fila)
         { 
-            this.tabla.Rows.Add(fila.Tiempo, fila.VarDependiente, fila.K1, fila.K2, fila.K3, fila.K4, fila.TiempoSiguiente, fila.VarDependienteSiguiente);
+            this.tabla.Rows.Add(fila.Tiempo, fila.IndiceSecado, fila.K1, fila.K2, fila.K3, fila.K4, fila.TiempoSiguiente, fila.IndiceSecadoSiguiente);
         }
 
         internal class Fila
         {
             private double tiempo;
-            private double varDependiente;
+            private double indiceSecado;
             private double k1;
             private double k2;
             private double k3;
             private double k4;
             private double tiempoSiguiente;
-            private double varDependienteSiguiente;
+            private double indiceSecadoSiguiente;
 
             public double Tiempo { get => tiempo; set => tiempo = value; }
-            public double VarDependiente { get => varDependiente; set => varDependiente = value; }
+            public double IndiceSecado { get => indiceSecado; set => indiceSecado = value; }
             public double K1 { get => k1; set => k1 = value; }
             public double K2 { get => k2; set => k2 = value; }
             public double K3 { get => k3; set => k3 = value; }
             public double K4 { get => k4; set => k4 = value; }
             public double TiempoSiguiente { get => tiempoSiguiente; set => tiempoSiguiente = value; }
-            public double VarDependienteSiguiente { get => varDependienteSiguiente; set => varDependienteSiguiente = value; }
+            public double IndiceSecadoSiguiente { get => indiceSecadoSiguiente; set => indiceSecadoSiguiente = value; }
         }
     }
 }
